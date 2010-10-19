57749a37a167af94b68c19189b5062300e5807f9
pk424kp <pk424kp@gmail.com>
2010-10-19 17:15:08 +0000

    javadoc - DTOLibrary1

--- DTOLibrary1/src/DTO/VoteDTO.java
+++ DTOLibrary1/src/DTO/VoteDTO.java
@@ -1,19 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package DTO;
 
 import java.io.Serializable;
 
 /**
- *
+ * Defines Vote as a Data Transfer Object
  * @author defiler
  */
 public class VoteDTO implements Serializable {
+
     private String[] votedCandidates;
 
+    /**
+     * Creates a new VoteDTO for given voted candidates.
+     * @param votedCandidates array of  {@code String}s with voted candidates
+     */
     public VoteDTO(String[] votedCandidates) {
         this.votedCandidates = votedCandidates;
     }
@@ -21,10 +21,18 @@ public class VoteDTO implements Serializable {
     public VoteDTO() {
     }
 
+    /**
+     * Returns voted candidates.
+     * @return Array of {@code String}s with voted candidates.
+     */
     public String[] getVotedCandidates() {
         return votedCandidates;
     }
 
+    /**
+     * Sets voted candidates.
+     * @param votedCandidates array of {@code String}s with voted candidates.
+     */
     public void setVotedCandidates(String[] votedCandidates) {
         this.votedCandidates = votedCandidates;
     }
