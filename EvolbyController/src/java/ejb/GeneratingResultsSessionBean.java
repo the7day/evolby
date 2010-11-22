2b527512194200cbafc20305d4abe82d118a5600
Radqooo <Radqooo@gmail.com>
2010-11-22 09:26:43 +0000

    System teraz vie rozhodnut o vitazoch a porazenych

--- EvolbyController/src/java/ejb/GeneratingResultsSessionBean.java
+++ EvolbyController/src/java/ejb/GeneratingResultsSessionBean.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ejb;
 
 import DTO.ElectionEventResultDTO;
@@ -34,9 +33,10 @@ import pojos.ValidatorException;
  */
 @Stateless
 public class GeneratingResultsSessionBean implements GeneratingResultsSessionRemote {
+
     @EJB
     private ValidatorSessionRemote validatorSessionBean;
-    @PersistenceContext(unitName="EvolbyControllerPU")
+    @PersistenceContext(unitName = "EvolbyControllerPU")
     private EntityManager em;
     @EJB
     private CounterRemote counterBean;
@@ -45,10 +45,10 @@ public class GeneratingResultsSessionBean implements GeneratingResultsSessionRem
      * Finishes the given election event.
      * @param eventId id of the given election event
      */
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW )
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public void finishElectionEvent(final Integer eventId) throws ControllerException {
         ElectionEvent electionEvent = em.find(ElectionEvent.class, eventId);
-        if(electionEvent == null) {
+        if (electionEvent == null) {
             throw new ControllerException("Election event not found.");
         }
         try {
@@ -77,10 +77,26 @@ public class GeneratingResultsSessionBean implements GeneratingResultsSessionRem
      * @throws ControllerException if election event not found
      */
     public void generateResult(final Integer eventId) throws ControllerException {
+        int numberOfSenators = 1;
+        System.out.println("GENERATING RESULTS");
         ElectionEvent event = em.find(ElectionEvent.class, eventId);
-        if(event == null) {
+        if (event == null) {
             throw new ControllerException("Election event not found.");
         }
+        try {
+            //  List <Candidate> n = new ArrayList<Candidate>();
+            //n.addAll(event.getCandidates());
+            //System.out.println("The event has "+n.get(0).getLogin()+"as a candidate");
+        } catch (Exception ex) {
+            //System.out.println("I made a bo bo");
+        }
+
+        //@Todo:Artificial condition because of smallness of testing data
+        int minVotes = (int) (event.getVoters().size() * 0.5) + 1;
+        System.out.println("The minimum votes to succeed is " + minVotes);
+
+
+
         ElectionEventResultDTO resultDTO = null;
         try {
             resultDTO = counterBean.getElectionEventResult(eventId);
@@ -91,17 +107,86 @@ public class GeneratingResultsSessionBean implements GeneratingResultsSessionRem
         Candidate candidate;
         int size = resultDTO.getCandidates().length;
         System.out.println("ResultDTO size: " + size);
-        for(int i=0; i<size; i++) {
+        List<ElectionResult> ERListIN = new ArrayList<ElectionResult>();
+        List<ElectionResult> ERListOut = new ArrayList<ElectionResult>();
+        for (int i = 0; i < size; i++) {
             result = new ElectionResult();
             result.setElectionEvent(event);
             candidate = em.find(Candidate.class, resultDTO.getCandidates()[i]);
-            if(candidate == null) {
+            if (candidate == null) {
                 throw new ControllerException("Candidate not found.");
             }
             result.setCandidate(candidate);
             result.setVotes(resultDTO.getVotes()[i]);
+            if (result.getVotes() >= minVotes) {
+                result.setElected(2);
+            } else {
+                result.setElected(3);
+            }
             em.persist(result);
-            event.getElectionResults().add(result);
+            //    event.getElectionResults().add(result);
+            ERListIN.add(result);
+        }
+        //SORT
+        if (size > 0) {
+            int pos = 0;
+            int max = ERListIN.get(0).getVotes();
+            //   ElectionResult tmp = ERListIN.get(pos);
+            while (!ERListIN.isEmpty()) {
+                pos = 0;
+                max = ERListIN.get(pos).getVotes();
+                for (int i = 0; i < ERListIN.size(); i++) {
+                    if (max < ERListIN.get(i).getVotes()) {
+                        max = ERListIN.get(i).getVotes();
+                        pos = i;
+                    }
+                    //WILL NOT WORK UNTIL VOTE DIFFERENTIATION IS IMPLEMENTED
+
+                 /*
+                   UNCOMMENT AFTER VOTER ROLE IS IMPLEMENTED
+                  
+
+                  if (max == ERListIN.get(i).getVotes()) {
+                        ElectionResult attacker = ERListIN.get(i);
+                        ElectionResult defender = ERListIN.get(pos);
+                        int voteA = (attacker.getCandidate().getCandidateRole().equals("STUDENT")) ? attacker.getSVotes() : attacker.getTVotes();
+                        int voteD = (defender.getCandidate().getCandidateRole().equals("STUDENT")) ? defender.getSVotes() : defender.getTVotes();
+                        if (voteA > voteD) {
+                            max = attacker.getVotes();
+                            pos = i;
+                        }
+                        if (voteA == voteD) {
+                            int rand = (int) Math.round(Math.random() * 10);
+                            if (rand > 5) {
+                                max = attacker.getVotes();
+                                pos = i;
+                            }
+
+                        }
+                    }*/
+                }
+                ERListOut.add(ERListIN.get(pos));
+                ERListIN.remove(pos);
+            }
+            for (int i = 0; i < ERListOut.size(); i++) {
+                if (i == numberOfSenators) {
+                    break;
+                }
+                if (ERListOut.get(i).getElected() != 3) {
+                    ERListOut.get(i).setElected(1);
+                }
+
+
+            }
+
+            event.getElectionResults().addAll(ERListOut);
+
+
+
+
+        } else {
+            //EndOfSORT
+            event.getElectionResults().addAll(ERListIN);
         }
         em.persist(event);
     }
@@ -114,12 +199,12 @@ public class GeneratingResultsSessionBean implements GeneratingResultsSessionRem
      */
     public Collection<ElectionEvent> getEndedEvents(final String login) throws ControllerException {
         Voter voter = em.find(Voter.class, login);
-        if(voter == null) {
+        if (voter == null) {
             throw new ControllerException("Voter not found.");
         }
         Collection<ElectionEvent> eventsOut = new ArrayList<ElectionEvent>();
-        for(ElectionEvent event : voter.getElectionEvents()) {
-            if(event.getFinished()) {
+        for (ElectionEvent event : voter.getElectionEvents()) {
+            if (event.getFinished()) {
                 eventsOut.add(event);
             }
         }
@@ -134,16 +219,15 @@ public class GeneratingResultsSessionBean implements GeneratingResultsSessionRem
      */
     public Collection<ElectionResult> getElectionEventResults(final Integer eventId) throws ControllerException {
         ElectionEvent electionEvent = em.find(ElectionEvent.class, eventId);
-        if(electionEvent == null) {
+        if (electionEvent == null) {
             throw new ControllerException("Election event not found.");
         }
+        System.out.println("Fetched Election Event: " + electionEvent.getName());
         Collection<ElectionResult> electionResults = electionEvent.getElectionResults();
         List<ElectionResult> resList = new ArrayList<ElectionResult>();
         resList.addAll(electionResults);
-  
+
         electionResults.size(); // hack
         return electionResults;
     }
-
-
 }
--- EvolbyWeb2/web/voters/resultEvent.jsp
+++ EvolbyWeb2/web/voters/resultEvent.jsp
@@ -1,5 +1,7 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
 <%@taglib prefix="f" uri="http://java.sun.com/jsf/core"%>
 <%@taglib prefix="h" uri="http://java.sun.com/jsf/html"%>
+
 
 <jsp:include page="../header.jsp" />
 <f:view>
@@ -18,7 +20,16 @@
             </f:facet>
             <h:outputText value="#{item.votes}"/>
         </h:column>
+        <h:column>
+            <f:facet name="header">
+                <h:outputText value="Successful" />
+            </f:facet>
+            <h:outputText value="#{item.elected}"/>   
+
+            
+        </h:column>
     </h:dataTable>
+        <h:outputText value="1-Elected,2-not enough space, 3- did not satisfy conditions"/>
 </f:view>
 
 
